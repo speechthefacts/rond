f781b3cc3fb0b08f12deefe37c7dcd116274659e
speechthefacts <164207295+speechthefacts@users.noreply.github.com>
2024-04-12 16:17:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -36,11 +36,6 @@ app.post('/create-checkout-session', async (req, res) => {
             shipping_address_collection: {
                 allowed_countries: ['FR'], // Définissez les pays autorisés pour l'adresse de livraison
             },
-            shipping: {
-                address: {
-                    line1: req.body.adresseLivraison, // Utilisez l'adresse de livraison fournie dans la requête
-                },
-            },
             metadata: {
                 diametre: req.body.diametre, // Récupération du diamètre depuis le corps de la requête
                 hauteur: req.body.hauteur // Récupération de la hauteur depuis le corps de la requête
@@ -57,4 +52,5 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
 
